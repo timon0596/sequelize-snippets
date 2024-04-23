815996afe7f2d4553563349a1a2584a39705630e
timon0596 <38828705+timon0596@users.noreply.github.com>
2024-04-23 11:34:30 +0300

    Update sequelize.snippets.ts

--- sequelize.snippets.ts
+++ sequelize.snippets.ts
@@ -2,7 +2,9 @@
 //find all where the result of connection of two columns is like a given string
 model.findAll({where: {
         [Op.and]: [
+        //regular "where" section block 
           { IsArchived: false, IsDeleted: false },
+        //condition to find rows where the result of connection of two columns is like a given string
           connection.where(
             connection.fn(
               'concat',
